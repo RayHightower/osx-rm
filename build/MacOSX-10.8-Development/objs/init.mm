2f28ee208b3c6defde7a474f1b2ce1f8bd4aacac
Raymond T. Hightower <rth@wisdomgroup.com>
2013-05-09 14:05:30 -0500

    make sure .gitignore lists the right files

---
+++ build/MacOSX-10.8-Development/objs/init.mm
@@ -0,0 +1,43 @@
+extern "C" {
+    void ruby_sysinit(int *, char ***);
+    void ruby_init(void);
+    void ruby_init_loadpath(void);
+    void ruby_script(const char *);
+    void ruby_set_argv(int, char **);
+    void rb_vm_init_compiler(void);
+    void rb_vm_init_jit(void);
+    void rb_vm_aot_feature_provide(const char *, void *);
+    void *rb_vm_top_self(void);
+    void rb_rb2oc_exc_handler(void);
+    void rb_exit(int);
+void MREP_A85FF7660E1F4C6AB0698838E56C227A(void *, void *);
+void MREP_DEF1824380AC42A8A6AE95E2F52BE2BC(void *, void *);
+}
+
+extern "C"
+void
+RubyMotionInit(int argc, char **argv)
+{
+    static bool initialized = false;
+    if (!initialized) {
+	ruby_init();
+	ruby_init_loadpath();
+        if (argc > 0) {
+	    const char *progname = argv[0];
+	    ruby_script(progname);
+	}
+#if !__LP64__
+	try {
+#endif
+	    void *self = rb_vm_top_self();
+MREP_A85FF7660E1F4C6AB0698838E56C227A(self, 0);
+MREP_DEF1824380AC42A8A6AE95E2F52BE2BC(self, 0);
+#if !__LP64__
+	}
+	catch (...) {
+	    rb_rb2oc_exc_handler();
+	}
+#endif
+	initialized = true;
+    }
+}
