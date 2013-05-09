108fc48018a643b8bd4c0a7430259ce8e60e168b
Raymond T. Hightower <rth@wisdomgroup.com>
2013-05-09 13:13:56 -0500

    initial commit

---
+++ build/MacOSX-10.8-Development/objs/init.mm
@@ -0,0 +1,43 @@
+extern "C" {
+    void ruby_sysinit(int *, char ***);
+    void ruby_init(void);
+    void ruby_init_loadpath(void);
+    void ruby_script(const char *);
+    void ruby_set_argv(int, char **);
+    void rb_vm_init_compiler(void);
+    void rb_vm_init_jit(void);
+    void rb_vm_aot_feature_provide(const char *, void *);
+    void *rb_vm_top_self(void);
+    void rb_rb2oc_exc_handler(void);
+    void rb_exit(int);
+void MREP_1683B8FEAE0A41E4B25B2C5062EF4497(void *, void *);
+void MREP_44C152806995401BAC79037F6D442915(void *, void *);
+}
+
+extern "C"
+void
+RubyMotionInit(int argc, char **argv)
+{
+    static bool initialized = false;
+    if (!initialized) {
+	ruby_init();
+	ruby_init_loadpath();
+        if (argc > 0) {
+	    const char *progname = argv[0];
+	    ruby_script(progname);
+	}
+#if !__LP64__
+	try {
+#endif
+	    void *self = rb_vm_top_self();
+MREP_1683B8FEAE0A41E4B25B2C5062EF4497(self, 0);
+MREP_44C152806995401BAC79037F6D442915(self, 0);
+#if !__LP64__
+	}
+	catch (...) {
+	    rb_rb2oc_exc_handler();
+	}
+#endif
+	initialized = true;
+    }
+}
